e31ab7c082da6a3b76d68e76bc11554fce65c966
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-12-02 23:25:11 +0000

    add profile serializer

---
+++ profiles/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Profile
+
+"""
+Create serializer and add a Meta class that specifies which fields
+can be accessed by the user.
+"""
+class ProfilesSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image'
+        ]
