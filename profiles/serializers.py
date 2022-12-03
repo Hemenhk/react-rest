0529c33d9682654673b07d3ad05f255f29152143
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-12-03 10:01:07 +0000

    Create a view for a profile's detail

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,12 +5,14 @@ from .models import Profile
 Create serializer and add a Meta class that specifies which fields
 can be accessed by the user.
 """
+
+
 class ProfilesSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image'
-        ]
+                'id', 'owner', 'created_at', 'updated_at', 'name',
+                'content', 'image'
+                ]
