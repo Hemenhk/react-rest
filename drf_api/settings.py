412a2e704b645f50fe80b556177efbf16485f934
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-12-03 20:42:33 +0000

    add filters

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -52,10 +52,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'django_filters',
     'profiles',
     'posts',
     'comments',
     'likes',
+    'followers',
     "django_rename_app",
 ]
 
--- profiles/views.py
+++ profiles/views.py
@@ -1,49 +1,42 @@
-from django.http import Http404
-from rest_framework import status
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfilesSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfilesSerializer(
-            profiles, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    serializer_class = ProfilesSerializer
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile'
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'followed_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     A view of each profile's detail. Accessed through a primary key 
     """
-    serializer_class = ProfilesSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfilesSerializer(profile, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfilesSerializer(
-            profile, data=request.data, context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = ProfilesSerializer
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
