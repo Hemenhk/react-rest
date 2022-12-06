f3162aa315bac4cac3e29a0b5cda31b1090bf4e3
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-12-06 22:24:30 +0000

    Add code to settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', os.environ.get('poloweb-api.herokuapp.com')]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST', 'localhost')]
 
 
 # Application definition
